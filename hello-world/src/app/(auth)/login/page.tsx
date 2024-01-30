d5637831fcd0ace9bd5606e9fafc16a6841ed91b
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-01-30 15:04:33 +0200

    navigation by useRoute() hook from next/navigation

--- hello-world/src/app/(auth)/login/page.tsx
+++ hello-world/src/app/(auth)/login/page.tsx
@@ -1,6 +1,10 @@
+import LoginBtn from "@/app/components/LoginBtn";
 
 const LoginPage = () => (
-  <h1>LoginPage</h1>
+  <>
+    <h1>LoginPage</h1>
+    <LoginBtn />
+  </>
 );
 
 export default LoginPage;
---
+++ hello-world/src/app/components/LoginBtn/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const LoginBtn = () => {
+  const { refresh, replace, back, push, prefetch, forward } = useRouter();
+
+  function loginHandler() {
+    console.log("Logged in");
+
+    replace("/");
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={loginHandler}
+      className="border-blue-950 border-2 text-blue-950 rounded-md py-2 px-8 hover:text-white hover:bg-blue-950 hover:transition-colors"
+    >
+      Login
+    </button>
+  );
+};
+
+export default LoginBtn;
