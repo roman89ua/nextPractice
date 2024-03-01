8d8eed39e4b092f7b6357d2df398e7bd26b727ff
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-03-01 22:12:46 +0200

    two others project to have practice differences between app router and page router

--- route-handlers/src/app/page.tsx
+++ route-handlers/src/app/page.tsx
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers";
+import Image from "next/image";
 
 export default function Home() {
   const theme = cookies().get("theme")?.value;
-  let classes = "";
+  let classes: string;
   if (theme === "dark") {
     classes = "bg-black text-white";
   } else {
@@ -14,6 +15,12 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${classes}`}
     >
       Routes Practice
+      <Image
+        width={1000}
+        height={1000}
+        alt="mage"
+        src="https://www.southernliving.com/thmb/F6CmBtFFGbA86t7rlicydxNcDnQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1312619551-15706e976a2d4e48bfedbf92bd4421c2.jpg"
+      />
     </main>
   );
 }
